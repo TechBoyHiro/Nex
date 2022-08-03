da53ff6269b35c1c695a91324053f88f511be796
hiro <amir.m.sh.tech@gmail.com>
2022-08-03 12:40:48 -0400

    Define Datebase Models

--- api/models.py
+++ api/models.py
@@ -3,144 +3,192 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
-# UPWORK MODELS =>
+# MODELS =>
 
-
+# Done
 class Token(models.Model): # token assigns to every user
     token = models.CharField(max_length=128)
-    validdate = models.DateTimeField(default=datetime.now())
+    validdate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.token + ' ** ' + str(self.validdate)
+        return str(self.id) + ' ** ' + self.token + ' ** ' + str(self.validdate)
 
-
-class MainUser(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    nationalcode = models.CharField(max_length=10,blank=True,null=True)
-    age = models.IntegerField(default=0)
-    phone = models.CharField(max_length=11)
-    profilepic = models.ImageField(upload_to='UserProfiles',blank=True,null=True)
-    isauthenticated = models.BooleanField(default=False)
-    datejoin = models.DateTimeField(default=datetime.now())
-    token = models.OneToOneField(Token,on_delete=models.SET_NULL,null=True)
-
-    def __str__(self):
-        return self.user.username + ' ** ' + self.phone + ' ** ' + self.token.token
-
-
-class SMS(models.Model):
-    sms = models.CharField(max_length=5)
-    phone = models.CharField(max_length=11)
-    issued = models.DateTimeField(default=datetime.now())
-    valid = models.DateTimeField(blank=True,null=True)
-
-
-class City(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name + ' ** ' + self.description
-
-
+# Done
 class Category(models.Model):
     name = models.TextField()
     description = models.TextField()
     icon = models.ImageField(upload_to='CategoryIcons',blank=True,null=True)
 
     def __str__(self):
-        return self.name + ' ** ' + self.description
+        return str(self.id) + ' ** ' + self.name + ' ** ' + self.description
 
-
-class Freelancer(models.Model):
-    name = models.CharField(max_length=25)
-    description = models.TextField(blank=True,null=True)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True,blank=True)
-    password = models.TextField(blank=True,null=True)
-    token = models.OneToOneField(Token,on_delete=models.SET_NULL,null=True)
-    email = models.EmailField(blank=True,null=True)
-    age = models.IntegerField(blank=True,null=True)
-    phone = models.CharField(max_length=11,blank=True)
-    city = models.ForeignKey(City,on_delete=models.CASCADE,blank=True,null=True)
-    address = models.TextField(blank=True,null=True)
-    rate = models.FloatField(default=0.0)
-    level = models.IntegerField(default=1)
-    datejoin = models.DateTimeField(default=datetime.now())
-    profilepic = models.ImageField(upload_to='FreelancerProfiles',blank=True,null=True)
-    numberofjobs = models.IntegerField(default=0)
+# Done
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.TextField()
+    description = models.TextField()
+    icon = models.ImageField(upload_to='SubCategoryIcons', blank=True, null=True)
 
     def __str__(self):
-        return self.name + ' ** ' + self.token.token + ' ** ' + self.phone
+        return str(self.id) + ' ** ' + self.name + ' ** ' + self.description
+
+# Done
+class BusinessType(models.Model):
+    subcategories = models.ManyToManyField(SubCategory)
+    name = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.id) + ' ** ' + self.name
+
+# Done
+class Shop(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    token = models.OneToOneField(Token,on_delete=models.RESTRICT,null=True)
+    businesstype = models.ForeignKey(BusinessType,on_delete=models.RESTRICT,blank=True,null=True)
+    name = models.CharField(max_length=60)
+    address = models.TextField()
+    phone = models.CharField(max_length=11)
+    profilepic = models.ImageField(upload_to='ShopProfiles',blank=True,null=True)
+    datejoin = models.DateField(auto_now_add=True)
+    instalink = models.TextField()
+    website = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.id) + ' ** ' + self.user.username + ' ** ' + self.phone + ' ** ' + self.token.token
+
+# Done
+class City(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
 
 
+# Done
+class Freelancer(models.Model):
+    token = models.OneToOneField(Token,on_delete=models.RESTRICT,null=True)
+    city = models.ForeignKey(City,on_delete=models.RESTRICT,blank=True,null=True)
+    name = models.CharField(max_length=25)
+    description = models.TextField()
+    password = models.TextField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    address = models.TextField()
+    datejoin = models.DateField(auto_now_add=True)
+    profilepic = models.ImageField(upload_to='FreelancerProfiles',blank=True,null=True)
+
+    def __str__(self):
+        return str(self.id) + ' ** ' + self.name + ' ** ' + self.token.token + ' ** ' + self.phone
+
+# Done
 class FreeFile(models.Model):
-    file = models.FileField(upload_to='FreeFiles')
     freelancer = models.ForeignKey(Freelancer,on_delete=models.CASCADE)
-    description = models.TextField(blank=True,null=True)
+    file = models.FileField(upload_to='FreeFiles')
+    description = models.TextField()
 
     def __str__(self):
         return self.freelancer.name + ' ** ' + self.description
 
-
-class SubCategory(models.Model):
-    name = models.TextField()
+# Done
+class Group(models.Model):
+    subcategories = models.ManyToManyField(SubCategory)
+    followers = models.ManyToManyField(Shop)
+    name = models.CharField(max_length=20)
     description = models.TextField()
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
-    icon = models.ImageField(upload_to='SubCategoryIcons', blank=True, null=True)
+    rate = models.FloatField(default=2.5)
+    successfulnumbers = models.IntegerField(default=0)
+    datejoin = models.DateField(auto_now_add=True)
+    website = models.TextField()
+    instalink = models.TextField()
+    isapproved = models.BooleanField(default=False)
+    icon = models.ImageField(upload_to='GroupIcons',blank=True,null=True)
+
+# Done
+class GroupMember(models.Model):
+    freelancer = models.ForeignKey(Freelancer,on_delete=models.RESTRICT)
+    group = models.ForeignKey(Group,on_delete=models.RESTRICT)
+    isadmin = models.BooleanField(default=False)
+    datejoin = models.DateField(auto_now_add=True)
+    role = models.TextField()
+    share = models.FloatField()
+
+# Done
+class Gig(models.Model):
+    group = models.ForeignKey(Group,on_delete=models.RESTRICT)
+    subcat = models.ForeignKey(SubCategory,on_delete=models.RESTRICT)
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    rate = models.FloatField(default=2.5)
+    leastprice = models.FloatField(default=10)
 
     def __str__(self):
-        return self.name + ' ** ' + self.description
+        return str(self.id) + ' ** ' + self.title + ' ** ' + self.freelancer.name
 
-
-class Work(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    freelancer = models.ForeignKey(Freelancer,on_delete=models.CASCADE)
-    rate = models.FloatField(default=0.0)
-    numberofrates = models.IntegerField(default=0)
-    leastprice = models.IntegerField(default=10)
-    subcategory = models.ForeignKey(SubCategory,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title + ' ** ' + self.freelancer.name
-
-
-class WorkFile(models.Model):
-    file = models.FileField(upload_to='WorkFiles')
-    work = models.ForeignKey(Work, on_delete=models.CASCADE)
+# Done
+class GigFile(models.Model):
+    gig = models.ForeignKey(Gig,on_delete=models.RESTRICT)
+    file = models.FileField(upload_to='GigFiles')
     priority = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.work.title + ' ** ' + str(self.priority)
+        return str(self.id) + ' ** ' + self.work.title + ' ** ' + str(self.priority)
 
-
-class Tag(models.Model):
-    name = models.TextField()
-    works = models.ManyToManyField(Work)
-
-    def __str__(self):
-        return self.name
-
-
+# Done
 class Package(models.Model):
-    title = models.TextField()
-    description = models.TextField(blank=True,null=True)
-    price = models.FloatField()
-    work = models.ForeignKey(Work,on_delete=models.CASCADE)
+    gig = models.ForeignKey(Gig,on_delete=models.CASCADE)
+    name = models.CharField(max_length=15)
+    description = models.TextField()
+    price = models.FloatField(default=0)
     deliverytime = models.IntegerField(default=1)
     numberofrevisions = models.IntegerField(default=1)
 
     def __str__(self):
         return str(self.price) + ' ** ' + self.title + ' ** ' + self.work.title
 
+# Done
+class Order(models.Model):
+    shop = models.ForeignKey(Shop,on_delete=models.RESTRICT)
+    package = models.ForeignKey(Package,on_delete=models.RESTRICT)
+    date = models.DateTimeField(auto_now_add=True)
+    ispaid = models.BooleanField(default=False)
+    tracknumber = models.TextField()
+    deliverytime = models.DateField(auto_now_add=True)
+    description = models.TextField()
 
+# Done
+class Review(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.RESTRICT)
+    Package = models.ForeignKey(Package,on_delete=models.RESTRICT)
+    rate = models.FloatField(default=2.5)
+    description = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+
+
+# Done
 class PackageDetail(models.Model):
+    package = models.ForeignKey(Package,on_delete=models.CASCADE)
     key = models.TextField()
     value = models.TextField()
-    package = models.ForeignKey(Package,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.key + ' ** ' + self.value + ' ** ' + self.package.title
+
+# Done
+class Tag(models.Model):
+    gigs = models.ManyToManyField(Gig)
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# Done
+class SMS(models.Model):
+    sms = models.CharField(max_length=5)
+    phone = models.CharField(max_length=11)
+    issued = models.DateTimeField(auto_now_add=True)
+    valid = models.DateTimeField()
+
+
 
 
 
