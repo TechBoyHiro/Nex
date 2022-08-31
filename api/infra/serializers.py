1c686dce1dccc601ab47669e8810f4e96bde44bd
hiro <amir.m.sh.tech@gmail.com>
2022-08-31 13:16:47 -0400

    Add GroupGetSerializer and GetGroupDetails And other serializers and methods

--- api/infra/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from api.models import *
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id','name','description']
-    icon = serializers.FilePathField()
